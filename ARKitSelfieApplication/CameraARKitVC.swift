1e1e7ccf22103a43fcd75dc10f40a0abda9278db
muhammadaamirqasmani <30751626+muhammadaamirqasmani@users.noreply.github.com>
2017-12-06 20:12:52 +0500

    add collection view for models...

--- ARKitSelfieApplication/CameraARKitVC.swift
+++ ARKitSelfieApplication/CameraARKitVC.swift
@@ -12,15 +12,18 @@ import ARVideoKit
 import Photos
 
 
-class ARKitVC: UIViewController, ARSCNViewDelegate, RenderARDelegate, RecordARDelegate{
+class CameraARKitVC: UIViewController, ARSCNViewDelegate, RenderARDelegate, RecordARDelegate, UICollectionViewDataSource , UICollectionViewDelegate{
 
 
     
 
     @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var modelCollection: UICollectionView!
     
     let caprturingQueue = DispatchQueue(label: "capturingThread", attributes: .concurrent)
     var recorder:RecordAR?
+    let itemsArray: [String] = ["goku","cup", "vase", "boxing", "table"]
+    var selectedItem: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,29 +33,28 @@ class ARKitVC: UIViewController, ARSCNViewDelegate, RenderARDelegate, RecordARDe
         
         // Create a new scene
         let scene = SCNScene(named: "art.scnassets/goku.scn")!
-        
         // Set the scene to the view
         sceneView.scene = scene
         sceneView.scene.rootNode.scale = SCNVector3(0.2, 0.2, 0.2)
-        
-        // Initialize ARVideoKit recorder
+
+//         Initialize ARVideoKit recorder
         recorder = RecordAR(ARSceneKit: sceneView)
-        
+
         // Set the recorder's delegate
         recorder?.delegate = self
-        
+
         // Set the renderer's delegate
         recorder?.renderAR = self
-        
+
         // Configure the renderer to perform additional image & video processing 👁
         recorder?.onlyRenderWhileRecording = false
-        
+
         // Configure ARKit content mode. Default is .auto
         //recorder?.contentMode = .aspectFit
-        
+
         // Set the UIViewController orientations
         recorder?.inputViewOrientations = [.landscapeLeft, .landscapeRight, .portrait]
-        
+
         // Configure RecordAR to store media files in local app directory
         recorder?.deleteCacheWhenExported = false
     }
@@ -66,8 +68,55 @@ class ARKitVC: UIViewController, ARSCNViewDelegate, RenderARDelegate, RecordARDe
         sceneView.session.run(configuration)
         
         // Prepare the recorder with sessions configuration
-        recorder?.prepare(configuration)
+//        recorder?.prepare(configuration)
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ModelCell", for: indexPath) as! ModelCell
+        cell.modelImage.image = UIImage(named: "\(self.itemsArray[indexPath.row]).png")
+        print("\(self.itemsArray[indexPath.row]).png")
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        self.selectedItem = itemsArray[indexPath.row]
+        cell?.backgroundColor = UIColor.green
+    }
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.orange
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        
+        let planeNode = createPlane(withPlaneAnchor: planeAnchor)
+        
+        node.addChildNode(planeNode)
+    }
+    
+    func createPlane(withPlaneAnchor planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        let planeNode = SCNNode()
+        planeNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+        let gridMaterial = SCNMaterial()
+        gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+        plane.materials = [gridMaterial]
+        planeNode.geometry = plane
+        
+        return planeNode
+    }
+    
     // MARK: - Hide Status Bar
     override var prefersStatusBarHidden: Bool {
         return true
---
+++ ARKitSelfieApplication/Extension/ModelCell.swift
@@ -0,0 +1,14 @@
+//
+//  ModelCell.swift
+//  ARKitSelfieApplication
+//
+//  Created by admin on 06/12/2017.
+//  Copyright © 2017 MuhammadAamir. All rights reserved.
+//
+
+import UIKit
+
+class ModelCell: UICollectionViewCell {
+    
+    @IBOutlet weak var modelImage: UIImageView!
+}
